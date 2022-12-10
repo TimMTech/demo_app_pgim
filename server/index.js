20455ec634545b8f0b4d0a67460d23ee188a5048
Tim Mukhamedov <tmukhamedov97@gmail.com>
2022-12-09 22:03:19 -0500

    .

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.resolve(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 
 app.get("/api/auth", (req, res) => {
