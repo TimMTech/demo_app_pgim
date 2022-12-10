c36355bc7641c89d67bc3fb9ae8efe8647101b9f
Tim Mukhamedov <tmukhamedov97@gmail.com>
2022-12-09 21:21:01 -0500

    .

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 app.get("/auth", (req, res) => {
   var result = imagekit.getAuthenticationParameters();
@@ -27,7 +27,7 @@ app.get("/auth", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, "/build", "index.html"));
 });
 
 app.listen(PORT, () => {
