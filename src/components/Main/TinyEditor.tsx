cff28d9581c9f24a565b6992eb6634782e94d286
Tim Mukhamedov <tmukhamedov97@gmail.com>
2022-12-11 02:30:46 -0500

    Added Menubar to Editor

--- src/components/Main/TinyEditor.tsx
+++ src/components/Main/TinyEditor.tsx
@@ -35,31 +35,8 @@ const TinyEditor: React.FC<TinyEditorProps> = ({
             branding: false,
             statusbar: false,
             toolbar: false,
-            forced_root_block: '',
+            forced_root_block: "",
             remove_trailing_brs: true,
-            style_formats: [
-              {
-                title: "Image Left",
-                selector: "img",
-                styles: {
-                  float: "left",
-                  margin: "0 10px 0 10px",
-                },
-              },
-              {
-                title: "Image Right",
-                selector: "img",
-                styles: {
-                  float: "right",
-                  margin: "0 0 10px 10px",
-                },
-              },
-            ],
-            image_class_list: [
-              { title: "Left", value: "w-full float-left" },
-              { title: "Right", value: "w-full float-right" },
-            ],
-            plugins: ["lists", "emoticons", "media", "table", "link", "image"],
           }}
         />
       ) : (
@@ -72,34 +49,17 @@ const TinyEditor: React.FC<TinyEditorProps> = ({
             height: "100%",
             width: "100%",
             resize: false,
-            menubar: false,
+            menubar: true,
             branding: false,
             statusbar: false,
             contextmenu: "copy paste",
-            forced_root_block: '',
+            forced_root_block: "",
             remove_trailing_brs: true,
             toolbar:
-              "undo redo | image | link | sizeselect | styles  | fontfamily | fontsize  | bullist | numlist | emoticons | alignleft aligncenter alignright alignjustify | outdent indent | media",
+              "undo redo | image | link | sizeselect | styles  | fontfamily | fontsize  | bullist | numlist | emoticons | alignleft aligncenter alignright alignjustify | lineheight | outdent indent | media",
 
             plugins: ["lists", "emoticons", "media", "table", "link", "image"],
-            style_formats: [
-              {
-                title: "Image Left",
-                selector: "img",
-                styles: {
-                  float: "left",
-                  margin: "0 10px 0 10px",
-                },
-              },
-              {
-                title: "Image Right",
-                selector: "img",
-                styles: {
-                  float: "right",
-                  margin: "0 0 10px 10px",
-                },
-              },
-            ],
+
             image_class_list: [
               { title: "Left", value: "w-full float-left" },
               { title: "Right", value: "w-full float-right" },
